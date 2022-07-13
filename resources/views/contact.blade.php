03a220b72893bafe6d3c55f321939ca747aee81b
Rijwan Ansari <rijwan5g2022@gmail.com>
2022-07-13 04:41:07 +0000

    1.-Routes,-Views-&-Layouts

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('title')
+  Contact Page
+@endsection
+
+@section("content")
+<h1>Contact</h1>
+<p>This is the Content of Contact Page</p>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('title')
+  Home Page
+@endsection
+
+@section("content")
+<h1>Welcome To Laravel!</h1>
+<p>This is the Content of Home Page</p>
+@endsection
